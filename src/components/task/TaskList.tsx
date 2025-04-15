b67a1bcb2c6224d0b0f21cc2f1adc250e469c988
eugene <gianyrox@gmail.com>
2025-04-15 23:01:11 +0000

    Apr 15, 2025, 7:01 PM

--- src/components/task/TaskList.tsx
+++ src/components/task/TaskList.tsx
@@ -1,1 +1,35 @@
- 
+import React from 'react';
+import { Task } from '../../types';
+import TaskCard from './TaskCard';
+import styles from './TaskList.module.css';
+
+interface TaskListProps {
+  tasks: Task[];
+  onStatusChange?: (taskId: string, newStatus: any) => void;
+  showDetails?: boolean;
+}
+
+export const TaskList: React.FC<TaskListProps> = ({ 
+  tasks,
+  onStatusChange,
+  showDetails = false
+}) => {
+  if (tasks.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className={styles.taskList}>
+      {tasks.map(task => (
+        <TaskCard
+          key={task.id}
+          task={task}
+          onStatusChange={onStatusChange}
+          showDetails={showDetails}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TaskList; 
