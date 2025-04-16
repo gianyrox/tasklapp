b1469348e738086028671c7f9af9d440ceb8eb56
eugene <gianyrox@gmail.com>
2025-04-16 00:18:18 +0000

    Apr 15, 2025, 8:18 PM

--- src/database/seeds/user8.sql
+++ src/database/seeds/user8.sql
@@ -1,1 +1,10 @@
- 
+-- Seed file for user8
+\i src/database/seeds/function.sql
+
+-- Add Hannah Brown
+SELECT add_test_user(
+  'user8@example.com',
+  'user8',
+  'Hannah Brown',
+  'https://randomuser.me/api/portraits/women/4.jpg'
+); 
