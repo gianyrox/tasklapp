28294153be074cf8ee5139007077a5e4cf37203a
eugene <gianyrox@gmail.com>
2025-04-16 07:28:39 +0000

    Apr 16, 2025, 3:28 AM

--- src/components/task/TaskList.tsx
+++ src/components/task/TaskList.tsx
@@ -8,16 +8,18 @@ interface TaskListProps {
   onStatusChange?: (taskId: string, newStatus: any) => void;
   showDetails?: boolean;
   ownership?: 'self' | 'friend';
+  compact?: boolean;
 }
 
 export const TaskList: React.FC<TaskListProps> = ({ 
   tasks,
   onStatusChange,
   showDetails = false,
-  ownership = 'self'
+  ownership = 'self',
+  compact = false
 }) => {
   return (
-    <div className={styles.taskList}>
+    <div className={`${styles.taskList} ${compact ? styles.compactList : ''}`}>
       {tasks.map(task => (
         <TaskCard
           key={task.id}
@@ -25,6 +27,7 @@ export const TaskList: React.FC<TaskListProps> = ({
           onStatusChange={onStatusChange}
           showDetails={showDetails}
           ownership={ownership}
+          compact={compact}
         />
       ))}
     </div>
