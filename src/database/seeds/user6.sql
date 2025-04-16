0f1b9cb98a8904ce36f1482254285c1acf19817f
eugene <gianyrox@gmail.com>
2025-04-16 00:18:08 +0000

    Apr 15, 2025, 8:18 PM

--- src/database/seeds/user6.sql
+++ src/database/seeds/user6.sql
@@ -1,1 +1,10 @@
- 
+-- Seed file for user6
+\i src/database/seeds/function.sql
+
+-- Add Fiona Taylor
+SELECT add_test_user(
+  'user6@example.com',
+  'user6',
+  'Fiona Taylor',
+  'https://randomuser.me/api/portraits/women/3.jpg'
+); 
